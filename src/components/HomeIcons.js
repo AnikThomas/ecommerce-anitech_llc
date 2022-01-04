c6198d4d5669a6d2452f5b503da27f05cf390a22
Anik Thomas <aniek.sw@gmail.com>
2022-01-04 16:27:00 -0700

    updated

--- src/components/HomeIcons.js
+++ src/components/HomeIcons.js
@@ -6,27 +6,27 @@ const HomeIcon = (props)=>{
     return(
         
         <Jumbotron fluid className="jumbo2">
-                <Container fluid>
-                    <div className="container">
-                        <div className="row mb-0">
-                            <div className="col-md-4 text-center">
-                                <i className="fa fa-cogs fa-5x mb-2"></i>
-                                <h3>Quality Products</h3>
-                                <p className="lead">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis, unde perspiciatis.</p>
-                            </div>
-                            <div className="col-md-4 text-center">
-                                <i className="fa fa-globe fa-5x"></i>
-                                <h3>Serve Globally</h3>
-                                <p className="lead">Lorem ipsum dolor sit, amet Debitis, unde perspiciatis.</p>
-                            </div>
-                            <div className="col-md-4 text-center">
-                                <i className="fa fa-certificate fa-5x"></i>
-                                <h3>Certificate of Origin</h3>
-                                <p className="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum alias blanditiis.</p>
-                            </div>
+            <Container fluid>
+                <div className="container">
+                    <div className="row mb-0">
+                        <div className="col-md-4 text-center">
+                            <i className="fa fa-cogs fa-5x mb-2"></i>
+                            <h3>Quality Products</h3>
+                            <p className="lead">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis, unde perspiciatis.</p>
+                        </div>
+                        <div className="col-md-4 text-center">
+                            <i className="fa fa-globe fa-5x"></i>
+                            <h3>Serve Globally</h3>
+                            <p className="lead">Lorem ipsum dolor sit, amet Debitis, unde perspiciatis.</p>
+                        </div>
+                        <div className="col-md-4 text-center">
+                            <i className="fa fa-certificate fa-5x"></i>
+                            <h3>Certificate of Origin</h3>
+                            <p className="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum alias blanditiis.</p>
                         </div>
                     </div>
-                </Container>
+                </div>
+            </Container>
             </Jumbotron>
     )
 }
